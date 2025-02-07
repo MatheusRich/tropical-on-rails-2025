5c096d2bcfe4df15eb358fdf1eb74d88d21af27c
Matheus Richard <matheusrichardt@gmail.com>
2025-02-07 18:55:37 -0300

    Auto-commit: 2025-02-07 18:55:37 - Changes detected

--- parser/2.rb
+++ parser/2.rb
@@ -55,7 +55,7 @@ end
 assert_equal({type: :number, value: 1}, Interpreter.call("1"))
 assert_raises(/Expected a number, got a/) { Interpreter.call("a") }
 
-puts Interpreter.call("1 + 1 + 2")
+pp Interpreter.call("1 + 1 + 2")
 # assert_equal({}, Interpreter.call("1 + 1 + 2"))
 # puts Interpreter.call("1 + 1")
 # assert_equal({}, Interpreter.call("1 + 1"))
