3d81aeb53dcf25f1e87cf265c8a6094aa9b9a9da
Matheus Richard <matheusrichardt@gmail.com>
2025-02-07 18:42:31 -0300

    Auto-commit: 2025-02-07 18:42:31 - Changes detected

---
+++ parser/1 copy.rb
@@ -0,0 +1,40 @@
+require_relative "helper"
+
+module Tokenizer
+  def self.call(input)
+    input.split
+  end
+end
+
+class Parser
+  def initialize(tokens)
+    @tokens = tokens
+  end
+
+  def call
+    program
+  end
+
+  def program
+    number
+  end
+
+  def number
+    token = @tokens.shift
+    raise "Expected a number, got #{token}" unless token.match?(/\A\d\z/)
+
+    {type: :number, value: token.to_i}
+  end
+end
+
+module Interpreter
+  def self.call(input)
+    tokens = Tokenizer.call(input)
+    Parser.new(tokens).call
+  end
+end
+
+assert_equal({type: :number, value: 1}, Interpreter.call("1"))
+assert_raises(/Expected a number, got a/) { Interpreter.call("a") }
+
+puts "All tests pass"
