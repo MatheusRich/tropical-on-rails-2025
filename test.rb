61bc8e4650be89c20a1a51eed934ba7d488caec4
Matheus Richard <matheusrichardt@gmail.com>
2025-02-25 17:59:30 -0300

    Auto-commit: 2025-02-25 17:59:30 - Changes detected

--- test.rb
+++ test.rb
@@ -1,5 +1,2 @@
-# list the files in the parser/ directory and run the second to last one (alphabetically)
-
 require 'fileutils'
-ruby_files = Dir.glob("parser/*.rb").sort
-system("ruby #{ruby_files[-2]}")
+ruby_files = Dir.glob("parser/*.rb").sort.reject { it.include?("helper") }
