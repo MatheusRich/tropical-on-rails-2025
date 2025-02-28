88a302b060312a2e2f3961c19553b2ae6a6bdb67
Matheus Richard <matheusrichardt@gmail.com>
2025-02-28 18:27:01 -0300

    Auto-commit: 2025-02-28 18:27:01 - Changes detected

--- interpreter/5.rb
+++ interpreter/5.rb
@@ -154,6 +154,7 @@ else
   assert_raises(/Expected a number, got \)/) { Interpreter.parse(")") }
 
   # Interpreter tests
+  assert_equal(3, Interpreter.call("1 + 2"))
 
   puts "All tests pass"
 end
