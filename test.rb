5d42ccd38e49648793f16e6bcf1a8405d3414f5c
Matheus Richard <matheusrichardt@gmail.com>
2025-03-07 17:21:34 -0300

    Auto-commit: 2025-03-07 17:21:34 - Changes detected

--- test.rb
+++ test.rb
@@ -3,5 +3,5 @@ ruby_files = Dir.glob("interpreter/*.rb").sort.reject { it.include?("helper") }
 
 ruby_files.each do |file|
   puts "Running #{file}"
-  system("ruby #{file}")
+  system("ruby #{file}") or exit(1)
 end
